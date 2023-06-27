283eaf16952530fa8c27bcd20666f5d3f784979e
Ivanleonov1 <geep1.ivan@gmail.com>
2023-06-27 16:16:06 +0700

    new Commit

--- src/main/java/com/example/convertxmltojson/services/XmlToJsonService.java
+++ src/main/java/com/example/convertxmltojson/services/XmlToJsonService.java
@@ -1,2 +1,29 @@
-package com.example.convertxmltojson.services;public class XmlToJsonService {
+package com.example.convertxmltojson.services;
+
+import org.springframework.stereotype.Service;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Service
+public class XmlToJsonService {
+    public String convertXmlToJson(String xml) {
+        // Формирование JSON из XML
+        String json = "{\"xml\": \"" + xml + "\"}";
+
+        sendJson(json);
+
+        return json;
+    }
+
+    private void sendJson(String json) {
+        try (FileWriter writer = new FileWriter("log.txt", true)) {
+            writer.write(("Received JSON: " + json));
+            writer.write(System.lineSeparator());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Received JSON: " + json);
+    }
 }
